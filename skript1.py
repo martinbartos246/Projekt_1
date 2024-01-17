307c9943deef638d8d91f4adb93cf5bd940a64b8
martin <martin.bartos246@gmail.com>
2024-01-17 15:11:01 +0100

    definice jednotek

---
+++ skript1.py
@@ -0,0 +1,24 @@
+
+#Prevadec jednotek "cviceni
+#Definice jednotek
+kg_pocet=0.264172
+km_pocet=0,621371
+l_pocet=0,2641720
+
+#Prevod
+cislo = input()
+print(cislo)
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
